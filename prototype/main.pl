7277f52e2620b8af7b5f7abbd41a3a90280b25cd
jingmi <nosqldev@gmail.com>
2015-05-18 19:52:15 +0800

    Basic framework of transaction

--- prototype/main.pl
+++ prototype/main.pl
@@ -20,16 +20,16 @@ use POSIX ":sys_wait_h";
 
 our @g_pipes = ();
 
-our $machine_cnt = 0;
+our $node_cnt = 0;
 
-our %g_machine_list = (
+our %g_node_list = (
     "m00" => {
         "id"  => undef,
         "pid" => undef,
     },
 );
 
-our %g_pid_machine_name = ();
+our %g_pid_node_name = ();
 
 # }}}
 
@@ -39,40 +39,42 @@ our %g_pid_machine_name = ();
 
 sub get_read_fd
 {
-    my $machine_id = shift;
+    my $node_id = shift;
 
-    return @g_pipes[$machine_id * 2];
+    return @g_pipes[$node_id * 2];
 }
 
 # }}}
 # {{{ sub get_write_fd
-# Description: get write fd by machine_id
+# Description: get write fd by node_id
 
 sub get_write_fd
 {
-    my $machine_id = shift;
+    my $node_id = shift;
 
-    return @g_pipes[$machine_id * 2 + 1];
+    return @g_pipes[$node_id * 2 + 1];
 }
 
 #}}}
 # {{{ sub send_msg
+
 sub send_msg
 {
-    my $machine_name = shift;
+    my $node_name = shift;
     my $cmd = shift; # string, length must be less than 16
     my $msg = shift; # serialized by Storable
 
     if (defined($msg))
     {
-        syswrite(&get_write_fd($g_machine_list{$machine_name}{"id"}), pack("A16N", $cmd, length($msg)));
-        syswrite(&get_write_fd($g_machine_list{$machine_name}{"id"}), $msg);
+        syswrite(&get_write_fd($g_node_list{$node_name}{"id"}), pack("A16N", $cmd, length($msg)));
+        syswrite(&get_write_fd($g_node_list{$node_name}{"id"}), $msg);
     }
     else
     {
-        syswrite(&get_write_fd($g_machine_list{$machine_name}{"id"}), pack("A16N", $cmd, 0));
+        syswrite(&get_write_fd($g_node_list{$node_name}{"id"}), pack("A16N", $cmd, 0));
     }
 }
+
 #}}}
 # {{{ sub recv_msg
 sub recv_msg
@@ -100,8 +102,8 @@ sub recv_msg
 # {{{ Setup Env
 # In this section, all functions are called by operator / client
 
-# {{{ sub launch_all_machines
-sub launch_all_machines
+# {{{ sub launch_all_nodes
+sub launch_all_nodes
 {
     my $cnt = shift;
 
@@ -114,46 +116,46 @@ sub launch_all_machines
         push(@g_pipes, $r, $w);
     }
 
-    &launch_machine for (1...$cnt);
-    select undef, undef, undef, 0.2;
+    &launch_node for (1...$cnt);
 }
 #}}}
-# {{{ sub launch_machine
+# {{{ sub launch_node
 
-sub launch_machine
+sub launch_node
 {
-    my $machine_name = sprintf "m%02d", $machine_cnt;
+    my $node_name = sprintf "m%02d", $node_cnt;
 
     my $pid = fork;
     if ($pid == 0)
     {
-        print color("green") . "[$$] ". $machine_name . " launched" . color("clear"), "\n";
-        &worker($machine_cnt);
+        print color("green") . "[$$] ". $node_name . " launched" . color("clear"), "\n";
+        &worker($node_cnt);
         exit;
     }
     else
     {
         select undef, undef, undef, 0.1;
-        $g_machine_list{$machine_name}{"pid"} = $pid;
-        $g_machine_list{$machine_name}{"id"} = $machine_cnt;
-        $g_pid_machine_name{$pid} = $machine_name;
-        $machine_cnt ++;
+        $g_node_list{$node_name}{"pid"} = $pid;
+        $g_node_list{$node_name}{"id"} = $node_cnt;
+        $g_pid_node_name{$pid} = $node_name;
+        $node_cnt ++;
     }
 }
 
 #}}}
-# {{{ sub wait_all_machine
-sub wait_all_machine
+# {{{ sub wait_all_node
+sub wait_all_node
 {
-    map { waitpid($_, 0) } keys %g_pid_machine_name;
+    map { waitpid($_, 0) } keys %g_pid_node_name;
 }
 #}}}
-# {{{ sub load_machine_info
+# {{{ sub load_node_info
 
-sub load_machine_info
+sub load_node_info
 {
-    my $buf = freeze \%g_machine_list;
-    map { &send_msg($_, "LOAD_MACHINE_INFO", $buf) } keys %g_machine_list;
+    my $buf = freeze \%g_node_list;
+    map { &send_msg($_, "LOAD_CONF", $buf) } keys %g_node_list;
+    select undef, undef, undef, 0.2;
 }
 
 #}}}
@@ -163,44 +165,77 @@ sub load_machine_info
 # {{{ Cluster Instance Functions
 
 # {{{ sub worker
-# Description: Instance of one machine, simulate the process on that machine
+# Description: Instance of one node, simulate the process on that node
 
 sub worker
 {
-    my $machine_id = shift;
+    my $node_id = shift;
 
-    my %kv = (); # simulate local data store on one machine
-    my %machine_list = (); # local hash to store all machines list
-    my %other_machine_list = (); # local machines list except self
-
-    my @add_events = ();
+    my %kv = (); # simulate local data store on one node
+    my %node_list = (); # local hash to store all nodes list
+    my %other_node_list = (); # local nodes list except self
 
     while (1)
     {
-        my ($cmd, $arg_ref) = &recv_msg( &get_read_fd($machine_id) );
+        my ($cmd, $arg_ref) = &recv_msg( &get_read_fd($node_id) );
 
-        print color("blue") . "[$$ : $machine_id] $cmd" . color("clear") . "\n";
+        print color("blue") . "[$$ : $node_id] $cmd" . color("clear") . "\n";
 
         if ($cmd eq 'EXIT')
         {
-            print color("red") . "[$$ : $machine_id] exit" . color("clear") . "\n";
+            #print color("red") . "[$$ : $node_id] exit" . color("clear") . "\n";
             return;
         }
         elsif ($cmd eq 'ADD')
         {
-            print color("cyan") . "[$$ : $machine_id] ADD ITEM" . color("clear") . "\n";
-            $kv{ $arg_ref->{'key'} } = $arg_ref->{'value'};
+            print color("cyan") . "[$$ : $node_id] ADD ITEM" . color("clear") . "\n";
+            my $event = &writeahead_event_add($arg_ref->{'key'}, $arg_ref->{'value'}, \%other_node_list);
+            &exec_transaction($event);
         }
         elsif ($cmd eq 'ALLKEYS')
         {
-            print color("green") . "[$$ : $machine_id] SHOW ALL KEYS" . color("clear") . "\n";
-            print Dumper \%kv;
+            print color("green") . "[$$ : $node_id] KEYS CNT: " . length(keys %kv) . color("clear") . "\n";
+            #print Dumper \%kv;
         }
-        elsif ($cmd eq 'LOAD_MACHINE_INFO')
+        elsif ($cmd eq 'LOAD_CONF')
         {
-            %machine_list = %{$arg_ref};
-            %other_machine_list = map { $_ => $machine_list{$_} } grep { $machine_list{$_}{'id'} != $machine_id } keys %machine_list;
+            %node_list = %{$arg_ref};
+            %other_node_list = map { $_ => $node_list{$_} } grep { $node_list{$_}{'id'} != $node_id } keys %node_list;
         }
+    }
+}
+
+sub writeahead_event_add
+{
+    my $key = shift;
+    my $value = shift;
+    my $other_node_list = shift;
+
+    my @other_node_list = sort {$a cmp $b} keys %{$other_node_list};
+
+    my %event_item = (
+        'action' => 'add',
+        'key' => $key,
+        'value' => $value,
+        'other_node_list' => \@other_node_list,
+    );
+
+    return \%event_item;
+}
+
+sub exec_transaction
+{
+    my $event = shift;
+
+    &prepare_transaction($event);
+    #&commit_transanction;
+}
+
+sub prepare_transaction
+{
+    my $event = shift;
+    foreach my $node_name (@{$event->{'other_node_list'}})
+    {
     }
 }
 
@@ -212,8 +247,8 @@ sub worker
 
 sub main
 {
-    &launch_all_machines(3);
-    &load_machine_info;
+    &launch_all_nodes(3);
+    &load_node_info;
 
     my $arg = freeze {'key'=>'city', 'value'=>'beijing'};
 
@@ -224,7 +259,7 @@ sub main
     &send_msg("m01", "EXIT", undef);
     &send_msg("m02", "EXIT", undef);
 
-    &wait_all_machine;
+    &wait_all_node;
 }
 
 __END__
